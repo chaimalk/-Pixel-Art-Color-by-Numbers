807faa77cf9141b2c0e92b5fc81ef17efb2d6f63
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-20 00:09:52 +0100

    Create PixelArt.ads

---
+++ PixelArt.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-1010770342437944/1711074800",
+    "admobInterstitial": "ca-app-pub-1010770342437944/2832584800",
+    "applovinBanner": "ae001894b7d88935",
+    "applovinInterstitial": "4d8fe9a0e3c09fd3",
+    "unityGameId": "5172202",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
